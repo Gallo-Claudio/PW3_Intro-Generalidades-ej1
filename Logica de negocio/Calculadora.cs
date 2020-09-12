ddffeb679ef6fb72e1baf9a275738ab95db5560e
Gallo <g>
2020-09-12 19:35:41 -0300

    Prueba de conflicto

--- Logica de negocio/Calculadora.cs
+++ Logica de negocio/Calculadora.cs
@@ -8,10 +8,17 @@ namespace Logica_de_negocio
 {
     public class Calculadora
     {
+        /// <summary>
+        /// Sumar
+        /// </summary>
+        /// <param name="ingreso1"></param>
+        /// <param name="ingreso2"></param>
+        /// <returns></returns>
         public static int Sumar(string ingreso1, string ingreso2)
         {
             int primerValor = int.Parse(ingreso1);
             int segundoValor = int.Parse(ingreso2);
+            int tercerValor = 10 * primerValor;
 
             int resultado = primerValor + segundoValor;
             return resultado;
